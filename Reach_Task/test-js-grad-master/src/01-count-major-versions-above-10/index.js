9bab323965117de697958fd2504d90e22e6cd8a2
TreCodes <94795862+TreCodes@users.noreply.github.com>
2021-11-21 18:10:20 +0000

    Update index.js

--- Reach_Task/test-js-grad-master/src/01-count-major-versions-above-10/index.js
+++ Reach_Task/test-js-grad-master/src/01-count-major-versions-above-10/index.js
@@ -1,18 +1,13 @@
+
 /**
  * Make the following POST request with either axios or node-fetch:
-
 POST url: http://ambush-api.inyourarea.co.uk/ambush/intercept
 BODY: {
     "url": "https://api.npms.io/v2/search/suggestions?q=react",
     "method": "GET",
     "return_payload": true
 }
-
-
-a
-
  *******
-
 The results should have this structure:
 {
     "status": 200.0,
@@ -24,40 +19,37 @@ The results should have this structure:
       ...
     ]
 }
-
  ******
-
  *  With the results from this request, inside "content", count
  *  the number of packages that have a MAJOR semver version 
  *  greater than 10.x.x
  */
 
- const axios = require('axios');
+const axios = require('axios');
 
 module.exports = async function countMajorVersionsAbove10() {
-  const response = await axios({
-                    method: 'POST',
-                    data: {
-                      "url": "https://api.npms.io/v2/search/suggestions?q=react",
-                      "method": "GET",
-                      "return_payload": true
-                    },
-                    "url": 'http://ambush-api.inyourarea.co.uk/ambush/intercept',
-                  });
+    let count = 0;
+    try {
+        const response = await axios.post('http://ambush-api.inyourarea.co.uk/ambush/intercept', {
+            "url": "https://api.npms.io/v2/search/suggestions?q=react",
+            "method": "GET",
+            "return_payload": true
+        })
 
-    const data = res.data;
-    const content = data.content;
-    const packages = content.map((c) => c.package);
-    const versions = packages.map((p) => p.version);
-  
-    const count = versions.reduce((sum, version) => {
-      const ver = version.split('.');
-      const majorVer = ver[0];
-      if (+majorVer > 10) {
-        return sum + 1;
-      }
-      return sum;
-    }, 0);
+        const data = res.data;
+        const content = data.content;
 
-    return count;
+        for (let i = 0; i < content.length; i++) 
+        {
+            let version =  parseInt(content[i].package.version);
+            if (version > 10) 
+            {
+                count ++
+            }
+        }
+        return count;
+
+    } catch(err) {
+        console.error(err)
+    }
 };
